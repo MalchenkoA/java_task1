862c4c99c201c5d7e5c9f514c63b77b98a2da151
MalchenkoA <62177369+MalchenkoA@users.noreply.github.com>
2020-03-15 19:42:28 +0300

    Домашнее задание №2

--- sandbox/src/main/java/HomeTask1.java
+++ sandbox/src/main/java/HomeTask1.java
@@ -1,6 +1,8 @@
-public class HomeTask1{
-	public static void main(String[] args){
+public class HomeTask1 {
+	public static void main(String[] args) {
 		System.out.println("My home task 1");
-	}
 
+		Point p = new Point(2,4,5,8);
+			System.out.println("Расстояние между двумя точками " + "= " + p.distance());
+	}
 }
