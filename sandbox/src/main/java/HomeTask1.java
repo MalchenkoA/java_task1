c56b2183a8363860cec90d3183003081bcf69477
MalchenkoA <62177369+MalchenkoA@users.noreply.github.com>
2020-03-16 21:12:05 +0300

    Скорректировано домашнее задание 2 , чтобы не попадала папка build

--- sandbox/src/main/java/HomeTask1.java
+++ sandbox/src/main/java/HomeTask1.java
@@ -2,7 +2,7 @@ public class HomeTask1 {
 	public static void main(String[] args) {
 		System.out.println("My home task 1");
 
-		Point p = new Point(2,4,5,8);
+		Point p = new Point(3,5,6,9);
 			System.out.println("Расстояние между двумя точками " + "= " + p.distance());
 	}
 }
--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -6,7 +6,7 @@ public class Point {
     public double x2;
     public double y2;
 
-    public Point(double x1, double y1,double x2, double y2){
+    public Point(double x1, double y1, double x2, double y2){
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
