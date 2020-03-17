41367fbbed6e250b4ab460fb56ed90de04b0081f
MalchenkoA <62177369+MalchenkoA@users.noreply.github.com>
2020-03-17 21:18:31 +0300

    Исправление замечаний по заданию 2

--- sandbox/src/main/java/HomeTask1.java
+++ sandbox/src/main/java/HomeTask1.java
@@ -2,7 +2,8 @@ public class HomeTask1 {
 	public static void main(String[] args) {
 		System.out.println("My home task 1");
 
-		Point p = new Point(3,5,6,9);
-			System.out.println("Расстояние между двумя точками " + "= " + p.distance());
+		Point p1 = new Point(3,5);
+    Point p2 = new Point(6,9);
+			System.out.println("Расстояние между двумя точками (" + p1.x + ";" + p1.y + ") и (" + p2.x + ";" + p2.y + ") = " + p1.distance(p2));
 	}
 }
