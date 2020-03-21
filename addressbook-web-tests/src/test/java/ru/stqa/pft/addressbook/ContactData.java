f25ece07df8f1587671267e76edf2543ec719a54
MalchenkoA <62177369+MalchenkoA@users.noreply.github.com>
2020-03-21 23:37:46 +0300

    тест для создания контакта в адресной книге

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,55 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstname;
+  private final String lastname;
+  private final String address;
+  private final String homephone;
+  private final String workphone;
+  private final String mobilephone;
+  private final String faxphone;
+  private final String enail;
+
+  public ContactData(String firstname, String lastname, String address, String homephone, String workphone, String mobilephone, String faxphone, String enail) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.address = address;
+    this.homephone = homephone;
+    this.workphone = workphone;
+    this.mobilephone = mobilephone;
+    this.faxphone = faxphone;
+    this.enail = enail;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getHomephone() {
+    return homephone;
+  }
+
+  public String getWorkphone() {
+    return workphone;
+  }
+
+  public String getMobilephone() {
+    return mobilephone;
+  }
+
+  public String getFaxphone() {
+    return faxphone;
+  }
+
+  public String getEnail() {
+    return enail;
+  }
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -21,7 +21,6 @@ public class GroupCreationTests {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.xpath("//form[@id='LoginForm']/label[2]")).click();
     wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys(password);
