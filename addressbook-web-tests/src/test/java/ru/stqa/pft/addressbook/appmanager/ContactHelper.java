cb113cadd71c86689c8df88fabf609e4fe9335a0
MalchenkoA <62177369+MalchenkoA@users.noreply.github.com>
2020-03-22 18:37:30 +0300

    тесты для модификации групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -36,4 +36,12 @@ public class ContactHelper extends HelperBase {
   public void deleteSelectedContacts() {
     click(By.xpath("//input[@value='Delete']"));
   }
+
+  public void initContactModification() {
+    click(By.xpath("//img[@alt='Edit']"));
+  }
+
+  public void submitContactModification() {
+    click(By.xpath("(//input[@name='update'])[2]"));
+  }
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactModificationTests extends TestBase {
+
+  @Test
+  public void testContactModification(){
+    app.getNavigationHelper().gotoContactPage();
+    app.getContactHelper().selectContact();
+    app.getContactHelper().initContactModification();
+    app.getContactHelper().fillContactForm(new ContactData("Nastya2", "Mal", "Moscow", "123456789", "147852369", "987654321", "963258741", "test1@test.com"));
+    app.getContactHelper().submitContactModification();
+    app.getNavigationHelper().gotoContactPage();
+  }
+}
