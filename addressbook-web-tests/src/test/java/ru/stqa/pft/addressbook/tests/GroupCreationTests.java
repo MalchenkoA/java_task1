05673b9bc440212ca2c082b89589dc0ae4190cd0
MalchenkoA <62177369+MalchenkoA@users.noreply.github.com>
2020-03-28 22:23:43 +0300

    Оптимизация заполнения форм

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -12,7 +12,7 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test41", "test42", "test43"));
+    app.getGroupHelper().fillGroupForm(new GroupData("test41", null, null));
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupPage();
   }
