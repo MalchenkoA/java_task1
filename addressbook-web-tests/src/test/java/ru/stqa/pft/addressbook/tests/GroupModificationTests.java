ff01f5ef4b846dcd5822deaa02855ec5009d92a4
MalchenkoA <62177369+MalchenkoA@users.noreply.github.com>
2020-03-29 18:18:32 +0300

    промежуточный результат

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -7,6 +7,9 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification(){
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("test41", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test81", "test82", "test83"));
