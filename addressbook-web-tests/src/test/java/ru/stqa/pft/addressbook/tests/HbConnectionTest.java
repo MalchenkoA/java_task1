789ec2c6859a6455eda1c94fffc9df9a8fc57fe6
MalchenkoA <62177369+MalchenkoA@users.noreply.github.com>
2020-04-25 17:02:09 +0300

    Лекция 7.2. Object Relational Mapping (ORM)

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTest.java
@@ -0,0 +1,42 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.List;
+
+public class HbConnectionTest {
+
+    private SessionFactory sessionFactory;
+
+    @BeforeClass
+    protected void setUp() throws Exception {
+        // A SessionFactory is set up once for an application!
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+        try {
+            sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+            // so destroy it manually.
+            StandardServiceRegistryBuilder.destroy( registry );
+        }
+    }
+
+    @Test
+    public void testHbConnection(){
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<GroupData> result = session.createQuery( "from GroupData" ).list();
+        for (GroupData group : result) {
+            System.out.println(group);
+        }
+        session.getTransaction().commit();
+        session.close();
+
+    }
+}
