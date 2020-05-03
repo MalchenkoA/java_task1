abf9804dafbd3893fd1518eab7fb3d6f6dbcf094
MalchenkoA <62177369+MalchenkoA@users.noreply.github.com>
2020-05-03 16:10:12 +0300

    Лекция 8.5. Передача файлов на удалённую машину по протоколу FTP

---
+++ mantis-tests/src/test/resources/config_inc.php
@@ -0,0 +1,11 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+$g_signup_use_captcha     = OFF;
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = 'dSAWiv6mhCCLuE8PhfU4/J5NeD3Ku8kgGJT885fsi4U=';
